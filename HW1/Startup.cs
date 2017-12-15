8c90023e0f01a8104ff72e345533c9e29c51f127
Eric <Eric@ERIC_WPAD>
2017-12-15 20:44:27 +0800

    加入專案檔案。

---
+++ HW1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HW1.Startup))]
+namespace HW1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
