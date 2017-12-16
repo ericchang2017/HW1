6391ae53bcace5b7190206f456f8a22ed0902a1c
Eric <Eric@ERIC_WPAD>
2017-12-16 08:48:01 +0800

    資料庫加上是否已刪除欄位，並修改View不顯示已刪除的資料

--- HW1/Controllers/客戶資料Controller.cs
+++ HW1/Controllers/客戶資料Controller.cs
@@ -23,7 +23,7 @@ namespace HW1.Controllers
         // GET: 客戶資料
         public ActionResult Index()
         {
-            return View(db.客戶資料.ToList());
+            return View(db.客戶資料.Where(p=>p.是否已刪除==false).ToList());
         }
 
         // GET: 客戶資料/Details/5
@@ -116,7 +116,8 @@ namespace HW1.Controllers
         public ActionResult DeleteConfirmed(int id)
         {
             客戶資料 客戶資料 = db.客戶資料.Find(id);
-            db.客戶資料.Remove(客戶資料);
+            //db.客戶資料.Remove(客戶資料);
+            客戶資料.是否已刪除 = true;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
